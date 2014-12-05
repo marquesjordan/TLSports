fa05b2d2375c44c057bb622bba2fa6146c959b74
Marques Jordan <marques.jordan81@gmail.com>
2014-12-05 10:30:16 -0800

    First Commit, set Pages Ctrl and routes for Pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trueleaguesports::Application.config.secret_key_base = 'b8a0e5983a0354d49c37839642c06c099ca22663ae9e7d37a8456910500e7bb8b91aadd03baa1f340d04b22315e1fcc1d5165a3f4b6cb3ff611a6db6cefdaed6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trueleaguesports::Application.config.session_store :cookie_store, key: '_trueleaguesports_session'
